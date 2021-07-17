c9030f31de29b69f1ba0dfc377f49887f78284c4
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-17 14:54:44 +0700

    Add project files.

---
+++ SK-Lib-cmath-floor-Bagian4__CPP/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    int x = 15;
+    double hasil;
+    hasil = floor(x);
+    cout << "Floor of " << x << " = " << hasil << endl;
+
+    _getch();
+    return 0;
+}
